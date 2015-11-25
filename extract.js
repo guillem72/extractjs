56d1ee2453e824c649b114bdf87cd2d882defde7
Guillem Lluch Moll <guillem72@gmail.com>
2015-11-25 17:16:35 +0100

    Create extract.js

---
+++ extract.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/*
+ * This script is for test extraction from a json file
+ * The object is call info
+ * 
+ * Creator: Guillem LLuch Moll guillem72 gmail.com
+ * Licence: public domain
+ * 
+ * */
+var fs=require("fs");
+if (process.argv.length<4)
+{
+	console.error ('usage: "nodejs extract.js JSON_FILE JSON_ROUTE"');
+}
+else
+{
+	var jsonName=process.argv[2];
+	var route=process.argv[3];
+ try {	var jsonFile=fs.readFileSync(jsonName);
+	var info=JSON.parse(jsonFile);
+	console.log(eval(route));} catch (e) {console.error(e)}
+	
+}
